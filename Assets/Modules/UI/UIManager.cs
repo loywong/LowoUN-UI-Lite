9679d620c215944ae4f7abb71cec2be2cd10b03e
LoyWong <loywongcom@gmail.com>
2026-01-18 21:08:40 +0800

    Refactor UI module, about removing UIRootController

--- Assets/Modules/UI/UIManager.cs
+++ Assets/Modules/UI/UIManager.cs
@@ -22,7 +22,7 @@ namespace LowoUN.Module.UI {
         public Canvas _worldCanvas { private set; get; }
         public void SetWorldCanvas (Canvas can) { _worldCanvas = can; }
 
-        public UIRootView RootView;
+        // public UIRootView RootView;
         public Canvas _screenCanvas;
         Transform hudLayer;
         Transform floorLayer;
@@ -45,18 +45,40 @@ namespace LowoUN.Module.UI {
         private Dictionary<string, UIViewBase> viewMap = new Dictionary<string, UIViewBase> ();
         // private UITipManager tipManager;
 
+        UIRootView _view;
         public void Init (Action cb_init) {
             Debug.Log ("------ UIManager --> Init");
-            UIRootController.Self.Init ();
-            Debug.Log ("UIRootController.Self.Init Over.");
+            
+            // UIRootController.Self.Init ();
+            GameObject h = Resources.Load<GameObject> ("UIRootView");
+            // ResUtils.LoadAsync_DontDestroy<GameObject>("UIRootView", (loadingHandler,h) =>
+            // {
+            GameObject hResult = h;
+            var canvas = GameObject.Find ("Canvas");
+            GameObject.DontDestroyOnLoad (canvas);
+            UIManager.Self._screenCanvas = canvas.GetComponent<Canvas> ();
+            var camCamvas = GameObject.Find ("CamCanvas");
+            GameObject.DontDestroyOnLoad (camCamvas);
+            UIManager.Self.SetCamCanvas (camCamvas.GetComponent<Canvas> ());
+            var worldCamvas = GameObject.Find ("WorldCanvas");
+            GameObject.DontDestroyOnLoad (worldCamvas);
+            UIManager.Self.SetWorldCanvas (worldCamvas.GetComponent<Canvas> ());
+
+            var gameObject = GameObject.Instantiate (hResult, canvas.transform);
+            gameObject.name = hResult.name;
+            _view = gameObject.GetComponent<UIRootView> ();
+
+            HideDownloading ();
+            // UIManager.Self.RootView = _view;
+            Debug.Log ("UIRootView Init Succ");
 
             // if (tipManager == null)
             //     tipManager = new UITipManager();
 
-            topLayer = RootView.TopLayer;
-            midLayer = RootView.MidLayer;
-            floorLayer = RootView.FloorLayer;
-            hudLayer = RootView.HUDRoot;
+            topLayer = _view.TopLayer;
+            midLayer = _view.MidLayer;
+            floorLayer = _view.FloorLayer;
+            hudLayer = _view.HUDRoot;
 
             cb_init.Invoke ();
         }
@@ -367,5 +389,53 @@ namespace LowoUN.Module.UI {
         }
 
         public bool ifCanvasShow => ParentLayer_SceneHud.gameObject.activeInHierarchy;
+
+        // /// <summary>
+        // /// 显示Toast提示信息
+        // /// </summary>
+        // void OnShowToast(string txt)
+        // {
+        //     _view.ShowToast(txt);
+        // }
+
+        public void ShowWaitingUI (bool isUsrClickUI) {
+            // Debug.LogError("UIRootController -- ShowWaitingUI");
+            _view.ShowWaitingUI (isUsrClickUI);
+        }
+        public void HideWaitingUI () {
+            // Debug.LogError("UIRootController -- HideWaitingUI");
+            _view.HideWaitingUI ();
+        }
+
+        public void ForceHideWaitingUI () {
+            // Debug.LogError("UIRootController -- ForceHideWaitingUI");
+#if !SRV_ALIYUN_PRODUCTION
+            Debug.Log ("[!SRV_ALIYUN_PRODUCTION] ------ ForceHideWaitingUI");
+#endif
+            _view.ForceHideWaitingUI ();
+        }
+        public void TEST_DESTROY_WaitingUI () {
+            _view.TEST_UnloadWaitingUI ();
+        }
+
+        public void ShowDownloading () {
+            _view.con_downloading.gameObject.SetActive (true);
+        }
+        public void HideDownloading () {
+            _view.txt_downloadingPercent.text = "";
+            _view.con_downloading.gameObject.SetActive (false);
+        }
+
+        string progressStr = "";
+        public void UpdateDownloading (float progress) {
+            if (progress < 0) progress = 0;
+            progress = (int) (progress * 100);
+            progressStr = $"{progress}%";
+
+#if UNITY_EDITOR
+            Debug.Log ($"progressStr:{progressStr}");
+#endif
+            _view.txt_downloadingPercent.text = progressStr;
+        }
     }
 }
